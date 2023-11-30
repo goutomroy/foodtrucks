f1770b1fd97e3ce5899c475cdcd4c2217dbb8fb8
goutomroy <goutom.roy@easysolar-app.com>
2023-11-30 13:09:13 +0100

    Modified hard coded user location and distance calculation is working

--- apps/trucks/views.py
+++ apps/trucks/views.py
@@ -3,8 +3,8 @@ from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 from .models import Truck
 
-longitude = 37.76008693198698
-latitude = -122.41880648110114
+longitude = -122.41880648110114
+latitude = 37.76008693198698
 user_location = Point(longitude, latitude, srid=4326)
 
 
